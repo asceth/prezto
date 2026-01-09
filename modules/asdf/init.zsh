6a20a64a359278b0d9ef0454f8ad429ee2419d85
John Long <john.long@rfa.sc.gov>
2026-01-09 12:57:30 -0500

    update some modules and asdf init

--- modules/asdf/init.zsh
+++ modules/asdf/init.zsh
@@ -7,10 +7,13 @@
 #
 
 # Load asdf into the shell session.
-if [[ -s "$HOME/.asdf/asdf.sh" ]]; then
-  source "$HOME/.asdf/asdf.sh"
-fi
+# if [[ -s "$HOME/.asdf/asdf.sh" ]]; then
+#   source "$HOME/.asdf/asdf.sh"
+# fi
+#
+# if (( $+commands[vg] )); then
+#   eval "$(vg eval --shell zsh)"
+# fi
 
-if (( $+commands[vg] )); then
-  eval "$(vg eval --shell zsh)"
-fi
+export ASDF_DATA_DIR="$HOME/.asdf"
+export PATH="$ASDF_DATA_DIR/shims:$PATH"
